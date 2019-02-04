dcbaeb0d04445a15fb5eaaad36d384b455a924ad
Michael Soares <mikey.soares@gmail.com>
2019-02-04 12:56:26 +0000

    Automatically backed up by Learn

---
+++ lib/board.rb
@@ -0,0 +1,34 @@
+class Board
+
+  attr_accessor :cells
+
+  def initialize
+    reset!
+
+  end
+
+  def reset
+    @cells = array.new(9, "")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position
+    input = gets.chomp.to_i
+    @cells[input-1]
+  end
+
+  def update(position, player)
+
+
+  end
+
+
+
+end
